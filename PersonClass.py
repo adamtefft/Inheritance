cd6936eddee0406225c0c99f7fc0bbc0cdc053ab
adamtefft <adam_tefft1@baylor.edu>
2023-02-28 10:51:18 -0600

    updated

--- PersonClass.py
+++ PersonClass.py
@@ -8,6 +8,20 @@ class Person:
     def print_person(self):
         print(self.name, self.address, self.phone)
 
+    def get_name(self):
+        return self.name
+
+    def get_address(self):
+        return self.address
+
+    def get_phone(self):
+        return self.phone
+
+    def print_person(self):
+        print("Name:", self.name)
+        print("Address:", self.address)
+        print("Phone:", self.phone)
+
 
 class Customer(Person):
     def __init__(self, name, address, phone, customerno, mailinglist):
@@ -18,3 +32,8 @@ class Customer(Person):
 
     def print_person(self):
         print(self.name, self.address, self.phone)
+
+        print(f"Customer Number: {self.customerno}")
+
+# Hidden attributes are not passed on in inheritance
+#
--- PersonProgram.py
+++ PersonProgram.py
@@ -1,7 +1,20 @@
 import PersonClass as pc
 
-person = pc.Person("Dad", "12103 Queensbury Ln", "713-366-2755")
-customer = pc.Customer("Mom", "12103 Quensbury Ln", "832-266-7534", 75, True)
+# define the variables
+name = "Dad"
+address = "12103 Queensbury Ln"
+phone_number = "713-366-2755"
+customerno = 1234
+mailinglist = True
 
-print(person.print_person())
-print(customer.print_person())
+# create an instance of the person class
+person = pc.Person(name, address, phone_number)
+
+# create an instance of the customer class
+customer = pc.Customer(name, address, phone_number, customerno, mailinglist)
+
+customer.print_person()
+
+
+# print(person.print_person())
+# print(customer.print_person())
